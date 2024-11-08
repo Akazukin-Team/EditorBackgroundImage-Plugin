6f0ec823655c23fafd71ea17266c03d34930f8c7
Currypan1229 <abcqqqwpvp@gmail.com>
2024-11-09 03:55:23 +0900

    Up to date

--- plugin/src/main/java/org/akazukin/intellij/background/EditorBackgroundImage.java
+++ plugin/src/main/java/org/akazukin/intellij/background/EditorBackgroundImage.java
@@ -9,7 +9,7 @@ public final class EditorBackgroundImage {
     public static final String PLUGIN_NAME = "EditorBackgroundImage";
     public static final String PLUGIN_ID = "editor_background_image";
     public static final String ACT_PLUGIN_ID = "org.akazukin.editorBackgroundImage";
-    public static final String PLUGIN_VERSION = "1.0.0";
+    public static final String PLUGIN_VERSION = "1.1.0";
 
     @Getter
     @Setter
