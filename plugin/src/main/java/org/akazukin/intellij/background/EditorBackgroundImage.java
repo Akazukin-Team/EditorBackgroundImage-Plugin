81aa7716af874e6c11f6608f54372d54ad5a7ffa
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2025-05-05 08:41:40 +0900

    Fix Dynamic plugin loader and enhance logging
    Revised plugin lifecycle management, and adding synchronization for thread safety.

--- plugin/src/main/java/org/akazukin/intellij/background/EditorBackgroundImage.java
+++ plugin/src/main/java/org/akazukin/intellij/background/EditorBackgroundImage.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.akazukin.intellij.background.settings.Config;
 import org.akazukin.intellij.background.task.BackgroundScheduler;
 import org.akazukin.intellij.background.task.SetRandomBackgroundTask;
@@ -14,6 +15,7 @@ import java.io.File;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
+@Slf4j
 public final class EditorBackgroundImage {
     public static final String PLUGIN_NAME_SPACE = "Editor Background Image";
     public static final String PLUGIN_NAME = "EditorBackgroundImage";
@@ -32,7 +34,12 @@ public final class EditorBackgroundImage {
         this.taskMgr.registerTasks();
     }
 
+    public boolean isEnabled() {
+        return this.scheduler.isScheduled();
+    }
+
     public void onEnable() {
+        log.info("Enabling " + PLUGIN_NAME_SPACE);
         final Config.State state = Config.getInstance();
         if (state.isAutoChangeEnabled()) {
             synchronized (this.scheduler) {
@@ -42,9 +49,12 @@ public final class EditorBackgroundImage {
                 }
             }
         }
+        log.info("Enabled " + PLUGIN_NAME_SPACE);
     }
 
-    public void onUnload() {
+    public void onDisable() {
+        log.info("Disabling " + PLUGIN_NAME_SPACE);
         this.scheduler.shutdown();
+        log.info("Disabled " + PLUGIN_NAME_SPACE);
     }
 }
